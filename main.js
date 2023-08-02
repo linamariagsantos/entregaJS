df43ac2cf15f6a5304fed76605817107d587a066
linamariagsantos <linamariag.santos@hotmail.com>
2023-08-02 14:28:41 -0500

    First commit

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function demo(x, y) {
 }
 
 function calculos(porcentaje, valor) {
+
     let prestamo = (porcentaje / 100) * valor
     alert("El prestamo solicitado es de: " + prestamo + " $")
 
@@ -30,40 +31,61 @@ function calculos(porcentaje, valor) {
     let base = 1 + tasa
     let valorActual = demo(base, nummeses)
 
-    let suma = prestamo * ((valorActual * tasa) / (valorActual - 1)) 
+    let ecuacion = prestamo * ((valorActual * tasa) / (valorActual - 1))
 
-    alert("La cuota mensual es de: "+ suma.toFixed(2))
+    alert("La cuota mensual es de: " + ecuacion.toFixed(2))
 
-    return suma
+    return ecuacion
+
 }
+
+function ComparativaCuotas(compraCasa,porcentaje) {
+    console.log("Comparativa de cuotas")
+
+    for (let i = 0; i < 7; i++) {
+
+        let prestamo = (porcentaje / 100) * valor
+        const nummeses = 360
+        const tasa = 0.0025
+        let base = 1 + tasa
+        let valorActual = demo(base, nummeses)
+
+        let ecuacion = prestamo * ((valorActual * tasa) / (valorActual - 1))
+
+        compraCasa[i] = ecuacion
+
+        console.log("Con el: " + porcentaje + "%, La cuota mes es de: " + compraCasa[i])
+
+        porcentaje = porcentaje + 10;
+    }
+}
+
 
 // 1. Valor de la vivienda
 let valor = parseInt(prompt("Ingresa el valor de la vivienda, sin puntos ni comas"))
 alert("El monto ingresado fue: " + valor)
 
 // 2. Valor del préstamo en %
-let porcentaje = parseInt(prompt("¿Cuanto dinero necesitas?: 10, 20, 30, 40, 50, 60 ó 70" + "%" + " (Ingresa solo el número)"))
+let continuar = false;
 
-if (porcentaje != 10 && porcentaje != 20 && porcentaje != 30 && porcentaje != 40 && porcentaje != 50 && porcentaje != 60 && porcentaje != 70) {
+while (continuar == false) {
 
-    let continuar = false;
+    let porcentaje = parseInt(prompt("¿Cuanto dinero necesitas?: 10, 20, 30, 40, 50, 60 ó 70 % (Ingresa solo el número)"))
 
-    alert("Valor incorrecto, por favor ingrese una de las opciones")
+    if (porcentaje == 10 || porcentaje == 20 || porcentaje == 30 || porcentaje == 40 || porcentaje == 50 || porcentaje == 60 || porcentaje == 70) {
 
-    while (continuar == false) {
+        continuar = true
 
-        let porcentaje = parseInt(prompt("¿Cuanto dinero necesitas?: 10, 20, 30, 40, 50, 60 ó 70" + "%" + " (Ingresa solo el número)"))
+        let resultado = calculos(porcentaje, valor).toFixed(2)
 
-        if (porcentaje == 10 || porcentaje == 20 || porcentaje == 30 || porcentaje == 40 || porcentaje == 50 || porcentaje == 60 || porcentaje == 70) {
-            continuar = true
+        let compraCasa = [resultado]
 
-            calculos(porcentaje,valor)            
+        /* A continuación se calcula el valor de la cuota correspondiente a cada porcentaje */
 
-        } else {
-            continuar = false;
-        }
+        porcentaje = 10
+        ComparativaCuotas(compraCasa, porcentaje)
+
+    } else {
+        continuar = false;
     }
-
-} else {
-    calculos(porcentaje,valor)
 }
